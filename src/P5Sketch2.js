489884506b7b87fad7591562de7a6e0aa6fd872e
fernandovmac <fernandovmac@gmail.com>
2020-03-20 21:01:16 +0100

    created an interactive sinewave ball shaped thingy for the P5 sketch

--- src/P5Sketch2.js
+++ src/P5Sketch2.js
@@ -12,34 +12,53 @@ class P5Canvas extends Component {
   }
 
   sketch = p => {
-    let r, g, b;
+    let xspacing = 10; // Distance between each horizontal location
+    let w; // Width of entire wave
+    let theta = 0.0; // Start angle at 0
+    let amplitude = 25.0; // Height of wave
+    let period = 200.0; // How many pixels before the wave repeats
+    let dx; // Value for incrementing x
+    let yvalues; // Using an array to store height values for the wave
 
     p.setup = () => {
-      p.createCanvas(200, 100);
-      p.background(200);
-
-      r = p.random(255);
-      g = p.random(255);
-      b = p.random(255);
+      p.createCanvas(280, 100);
+      w = 500;
+      dx = (1000 / period) * xspacing;
+      yvalues = new Array(p.floor(32));
     };
 
     p.draw = () => {
-      p.background(127);
-      // Draw a circle
-      p.strokeWeight(2);
-      p.stroke(r, g, b);
-      p.fill(r, g, b, 127);
-      p.ellipse(30, 30, 50, 50);
+      p.background(0);
+      p.calcWave();
+      p.renderWave();
     };
 
-    p.mousePressed = () => {
-      // Check if mouse is inside the circle
-      let d = p.dist(p.mouseX, p.mouseY, 50, 50);
-      if (d < 100) {
-        // Pick new random color values
-        r = p.random(255);
-        g = p.random(255);
-        b = p.random(255);
+    p.calcWave = () => {
+      // Increment theta (try different values for
+      // 'angular velocity' here)
+      theta += p.mouseX / 2000;
+
+      // For every x value, calculate a y value with sine function
+      let x = theta;
+      if (
+        p.mouseY >= 0 &&
+        p.mouseY <= p.height &&
+        p.mouseX >= 0 &&
+        p.mouseX <= p.width
+      ) {
+        for (let i = 0; i < yvalues.length; i++) {
+          yvalues[i] = p.sin(x) * (p.mouseY / 2);
+          x += dx;
+        }
+      }
+    };
+
+    p.renderWave = () => {
+      p.noStroke();
+      p.fill(255);
+      // A simple way to draw the wave with an ellipse at each location
+      for (let x = 0; x < yvalues.length; x++) {
+        p.ellipse(x * xspacing, 50 + yvalues[x], 16, 16);
       }
     };
   };
